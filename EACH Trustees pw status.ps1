bbe7fe590da2c5d51ed465620d84ec07203c36e4
KD-sat <60858517+KD-sat@users.noreply.github.com>
2020-04-27 14:48:39 +0100

    Update EACH Trustees pw status.ps1

--- EACH Trustees pw status.ps1
+++ EACH Trustees pw status.ps1
@@ -1,3 +1,3 @@
-﻿$users = Get-ADGroupMember -Identity "each trustees"
+﻿$users = Get-ADGroupMember -Identity "security group name"
 $(foreach ($user in $users){
 Get-ADUser $user -Properties msDS-UserPasswordExpiryTimeComputed, PasswordLastSet, CannotChangePassword | select Name, @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}, PasswordLastSet})
