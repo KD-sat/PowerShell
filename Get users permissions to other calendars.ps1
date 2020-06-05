dd184dff151edf55ddb22ee92b1924d01cd70e70
KD-sat <60858517+KD-sat@users.noreply.github.com>
2020-06-05 13:15:55 +0100

    Add files via upload

---
+++ Get users permissions to other calendars.ps1
@@ -0,0 +1,11 @@
+﻿ForEach ($mailbox in (Get-Mailbox)){
+    Get-Mailboxfolderpermission (($mailbox.PrimarySmtpAddress)+":\calendar") `
+        -User <target email address> -ErrorAction SilentlyContinue | ForEach-Object {
+        [PSCustomObject]@{
+            Identity     = $mailbox.Identity
+            User         = $_.User
+            AccessRights = $_.Accessrights
+            SharingPermissionFlags =$_.SharingPermissionFlags
+        }
+    }
+}
