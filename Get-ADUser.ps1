eb90a87c391eefb372c615ae65d333f4b45f4d74
kd-sat <kamildziama@gmail.com>
2020-10-11 15:34:13 +0100

    Amendments

--- Get-ADUser.ps1
+++ Get-ADUser.ps1
@@ -1,2 +1,3 @@
-﻿$oudn = "OU Distinguished Name"
-Get-ADUser -filter * -Properties * | Where-Object { $_.DistinguishedName -like "*,$oudn" } | select [list of all properties] https://social.technet.microsoft.com/wiki/contents/articles/12037.active-directory-get-aduser-default-and-extended-properties.aspx
+﻿#list of all properties - https://social.technet.microsoft.com/wiki/contents/articles/12037.active-directory-get-aduser-default-and-extended-properties.aspx
+$oudn = "OU Distinguished Name"
+Get-ADUser -filter * -Properties * | Where-Object { $_.DistinguishedName -like "*,$oudn" } | select 
