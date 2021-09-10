51d9c332e8512285af216136c2e3959cbd57c45c
KD-sat <60858517+KD-sat@users.noreply.github.com>
2021-09-10 13:36:06 +0100

    Add files via upload

---
+++ NTFS File Permissions Audit.ps1
@@ -0,0 +1,4 @@
+﻿## This script requires the NTFSSecurity PS Module available from PSGallery ##
+
+$paths = get-childitem -Path '#filepath - can be pointed to a drive' -Directory | % {$_.FullName}
+Get-NTFSAccess -Path $paths | Export-Csv -Path '#path'
