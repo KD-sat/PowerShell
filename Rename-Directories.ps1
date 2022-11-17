27bd69254eb0f59e9e77bef5cb48705f3e67b6d8
KD-sat <60858517+KD-sat@users.noreply.github.com>
2022-11-17 09:17:54 +0000

    Add files via upload

---
+++ Rename-Directories.ps1
@@ -0,0 +1,14 @@
+﻿
+$changes = Import-Csv .\changes.csv
+$dir = "change this to the directory path required"
+cd $dir
+$changes | ForEach-object {
+write-host "renaming " $($_.origDirName) "to " $($_.NewDirName)
+
+$oldname = $($_.origDirName)
+$newname = $($_.NewDirName)
+
+Get-ChildItem -Recurse -Directory -Path $dir -filter *$($_.origDirName)* | Rename-Item -NewName { $_.Name -replace $oldname , $newname } -verbose
+
+}
+
