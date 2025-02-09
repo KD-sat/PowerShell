03814290c31afa16db48c6dcc8d1800736819133
kd-sat <kamildziama@gmail.com>
2025-02-09 21:30:35 +0000

    Working on the script

--- Graph/Create-NewUsers.ps1
+++ Graph/Create-NewUsers.ps1
@@ -4,6 +4,7 @@
 #########################################################################
 #########################################################################
 
+#Parameters with new user details
 $path = "C:\Users\kamil\Desktop\Users.csv"
 $users = Import-CSV -Path $path
 $mailNickname = $users.displayName -replace " ","."
@@ -11,17 +12,29 @@ $upn = @()
 foreach ($mail in $mailnickname) {
     $upn += "$mail@notideal.co.uk"
 }
+$passwords = @()
 
+# Generating passwords to use with the accounts
+$uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+$lowercase = "abcdefghijklmnopqrstuvwxyz"
+$numbers = "0123456789"
+$symbols = "!@#$%^&*()"
+$allChars = $uppercase + $lowercase + $numbers + $symbols
 
+for ($i = 0; $i -lt 50; $i++) {
+    $password = -join ((1..16) | ForEach-Object { $allChars[(Get-Random -Maximum $allChars.Length)] })
+    $passwords += $password
+}
+
+#Mapping the params to the cmdlet parameters
 $params = @{
 	accountEnabled = $users.accountEnabled
 	displayName = $users.displayName
 	mailNickname = $mailNickname
 	userPrincipalName = $upn
-	passwordProfile = @{
-		forceChangePasswordNextSignIn = $true
-		password = 
-	}
+	forceChangePasswordNextSignIn = $true
+	password = $passwords
+	
 }
 
 Import-Module Microsoft.Graph.Users
